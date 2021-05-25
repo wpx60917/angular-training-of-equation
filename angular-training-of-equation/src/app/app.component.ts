ee796c1cbdaf199294f5711fba7ba9b4bcb84e4d
wpx60917 <44990316+wpx60917@users.noreply.github.com>
2021-05-25 18:58:11 +0800

    0525

--- angular-training-of-equation/src/app/app.component.ts
+++ angular-training-of-equation/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import * as d3 from "d3";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'training-of-equation';
 }
